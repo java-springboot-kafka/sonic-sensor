38a6b77d2ec74e5a35330787ca7b5656b753f1e7
mehrdad.karami <mehrdadallahkarami@gmail.com>
2023-11-05 22:59:08 +0100

    commit 3

--- src/sonic-sensor.cc
+++ src/sonic-sensor.cc
@@ -144,7 +144,7 @@ int main()
 {
   using namespace std;
   float distance;
-  std::cout << "Hello from C++ program!" << std::end;
+  //std::cout << "Hello from C++ program!" << std::end;
   //Initialize wiringPi
   wiringPiSetup();
 
