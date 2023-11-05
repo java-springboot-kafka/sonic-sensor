d7ec94120f9eb014c6eea8d23df5ac93edf0beee
mehrdad.karami <mehrdadallahkarami@gmail.com>
2023-11-05 22:34:27 +0100

    commit 2

---
+++ src/sonic-sensor.h
@@ -0,0 +1,10 @@
+// ultrasonic_sensor.h
+
+#ifndef ULTRASONIC_SENSOR_H
+#define ULTRASONIC_SENSOR_H
+
+float Distance();
+void bubble(unsigned long *a, int n);
+float Distance_test();
+
+#endif // ULTRASONIC_SENSOR_H
